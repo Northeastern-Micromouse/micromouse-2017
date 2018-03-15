f8d2aeb5e5f20f51f1ed4797452abfd579aee5dd
Noah Lichtenstein <lichto2005@gmail.com>
2018-03-15 19:53:20 -0400

    fix up some makefile stuff, add table reader
    - fix gitignore

--- firmware/src/main.cc
+++ firmware/src/main.cc
@@ -5,6 +5,7 @@
 #include "gpioDevice.h"
 #include "rgbLedDevice.h"
 #include "digitalPotentiometer.h"
+#include "tableReader.h"
 
 int main() {
     //micromouse::I2cDevice myI2C = micromouse::I2cDevice();
@@ -12,16 +13,24 @@ int main() {
     //65 47 64
     //46 44 124
 
-    micromouse::RgbLedDevice led(64, 47, 65);
-    led.setRgb(1, 1, 1);
+    //micromouse::RgbLedDevice led(64, 47, 65);
+    //led.setRgb(1, 1, 1);
+
+    micromouse::TableReader reader("test.csv");
+    auto table = reader.parseTableCSV();
+
+    for (int i = 0; i < table->size(); ++i) {
+        float val = table->at(i);
+        std::cout << val << std::endl;
+    }
+
+	//int pot0fd = open("/dev/spidev1.2", O_RDWR);
+	//std::cout << pot0fd << std::endl;
 	
-	int pot0fd = open("/dev/spidev1.2", O_RDWR);
-	std::cout << pot0fd << std::endl;
+	//micromouse::DigitalPotentiometer pot0(pot0fd);
 	
-	micromouse::DigitalPotentiometer pot0(pot0fd);
-	
-	std::cout << pot0.setResistance(1, 128) << std::endl;
-	std::cout << pot0.setResistance(2, 64) << std::endl;
+	//std::cout << pot0.setResistance(1, 128) << std::endl;
+	//std::cout << pot0.setResistance(2, 64) << std::endl;
 	
     /*
     micromouse::GpioDevice r(64);
